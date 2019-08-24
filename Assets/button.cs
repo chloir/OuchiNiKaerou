bdc28ac60e6fc8ed91fbd69e48e0d31fac212887
lastlound <54462855+lastlound@users.noreply.github.com>
2019-08-24 20:09:03 +0900

    シーン

---
+++ Assets/button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("押された!");  // ログを出力
+    }
+}
