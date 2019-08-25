53a8fbe33f43224ff13887795aab4f2b24bbe2ec
Reki Yamashita <chloir.sharp@gmail.com>
2019-08-25 11:35:59 +0900

    Title, Scene

--- Assets/Scripts/TitleSceneController.cs
+++ Assets/Scripts/TitleSceneController.cs
@@ -5,13 +5,34 @@ using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleSceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject gImage = null;
+    
     private void Start()
     {
         this.UpdateAsObservable()
             .Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(_ => ObservableGAnimator());
+    }
+
+    private void ObservableGAnimator()
+    {
+        Observable.FromCoroutine(GAnimator)
             .Subscribe(_ => SceneManager.LoadScene("SampleScene"));
+    }
+
+    public GameObject GetImageInstance() { return gImage; }
+
+    IEnumerator GAnimator()
+    {
+        var img = GetImageInstance();
+        while(img.transform.position.x < 300)
+        {
+            img.transform.Translate(10, 10, 0);
+            yield return null;
+        }
     }
 }
