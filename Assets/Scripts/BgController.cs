a02be462cf05893d2dcd3f60056851b17e8faffc
Reki Yamashita <chloir.sharp@gmail.com>
2019-08-25 13:28:42 +0900

    bg process changed

--- Assets/Scripts/BgController.cs
+++ Assets/Scripts/BgController.cs
@@ -8,8 +8,9 @@ using UnityEngine.UI;
 public class BgController : MonoBehaviour
 {
     [SerializeField] private SceneController _controller = null;
-    [SerializeField] private GameObject[] bgs = null;
-    [SerializeField] private Sprite[] bgSprites = null;
+    [SerializeField] private GameObject dogBg = null;
+    [SerializeField] private GameObject catBg = null;
+    [SerializeField] private GameObject gokiBg = null;
     
     void Start()
     {
@@ -19,25 +20,13 @@ public class BgController : MonoBehaviour
                 switch (_controller.GetState())
                 {
                     case SceneController.PlayerState.Dog:
-                        for (int i = 0; i < bgs.Length; i++)
-                        {
-                            var bg = bgs[i];
-                            bg.GetComponent<Image>().sprite = bgSprites[i];
-                        }
+                        dogBg.transform.Translate(Vector3.zero);
                         break;
                     case SceneController.PlayerState.Cat:
-                        for (int i = 0; i < bgs.Length; i++)
-                        {
-                            var bg = bgs[i];
-                            bg.GetComponent<Image>().sprite = bgSprites[i + 4];
-                        }
+
                         break;
                     case SceneController.PlayerState.Cockloach:
-                        for (int i = 0; i < bgs.Length; i++)
-                        {
-                            var bg = bgs[i];
-                            bg.GetComponent<Image>().sprite = bgSprites[i + 8];
-                        }
+                        
                         break;
                 }
             });
