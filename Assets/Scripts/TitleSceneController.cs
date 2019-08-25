9e1b78136d81b5b3632fdaa094578147f2d8f533
Reki Yamashita <chloir.sharp@gmail.com>
2019-08-25 10:58:45 +0900

    BgChanger Required

---
+++ Assets/Scripts/TitleSceneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneController : MonoBehaviour
+{
+    private void Start()
+    {
+        this.UpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(_ => SceneManager.LoadScene("SampleScene"));
+    }
+}
