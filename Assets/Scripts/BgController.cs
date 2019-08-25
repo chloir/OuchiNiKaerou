6e1d209a64e4e484e25f0b5bc6b2e17a2d9fd881
Reki Yamashita <chloir.sharp@gmail.com>
2019-08-25 16:09:01 +0900

    Gimmick Required

--- Assets/Scripts/BgController.cs
+++ Assets/Scripts/BgController.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 
 public class BgController : MonoBehaviour
 {
+    private static readonly int WIDTH = 1920;
+    private static readonly int HEIGHT = 1080;
     [SerializeField] private SceneController _controller = null;
     [SerializeField] private GameObject dogBg = null;
     [SerializeField] private GameObject catBg = null;
     [SerializeField] private GameObject gokiBg = null;
+    [SerializeField] private GameObject bgImg = null;
+    private int x = 0, y = 0;
 
     void Start()
     {
@@ -20,13 +24,16 @@ public class BgController : MonoBehaviour
                 switch (_controller.GetState())
                 {
                     case SceneController.PlayerState.Dog:
-                        dogBg.transform.position = Vector3.zero;
+                        x = _controller.GetBgPos();
+                        bgImg.transform.localPosition = new Vector3(x, 0);
                         break;
                     case SceneController.PlayerState.Cat:
-                        catBg.transform.position = Vector3.zero;
+                        x = _controller.GetBgPos();
+                        bgImg.transform.localPosition = new Vector3(x, 1080);
                         break;
                     case SceneController.PlayerState.Cockloach:
-                        gokiBg.transform.position = Vector3.zero;
+                        x = _controller.GetBgPos();
+                        bgImg.transform.localPosition = new Vector3(x, 2160);
                         break;
                 }
             });
